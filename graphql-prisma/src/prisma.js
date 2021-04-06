a775d01c63352422838822abb7d14ffe2cf93a70
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2021-04-06 09:25:45 -0300

    Finishing the orderBy sorting and pagination

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -1,23 +1,20 @@
-import { Prisma } from 'prisma-binding'
-import { fragmentReplacements } from "./resolvers/index"
+import { Prisma } from 'prisma-binding';
+import { fragmentReplacements } from './resolvers/index';
 
 const prisma = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://localhost:4466',
-    secret: "thisismysupersecrettext",
-    fragmentReplacements
-})
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466',
+  secret: 'thisismysupersecrettext',
+  fragmentReplacements,
+});
 
-
-
-export { prisma as default }
+export { prisma as default };
 
 // prisma.exists.Comment({
 //     id: "ckku2b7dt00yd0825aozipq8f"
 // }).then((exists) => {
 //     console.log(exists)
 // })
-
 
 // const createPostForUser = async (authorId, data) => {
 //     const userExists = await prisma.exists.User({
@@ -41,7 +38,6 @@ export { prisma as default }
 //     return post
 // }
 
-
 // const updatePostForUser = async (postId, data) => {
 //     const postExists = await prisma.exists.Post({ id: postId })
 
@@ -52,23 +48,22 @@ export { prisma as default }
 //     const post = await prisma.mutation.updatePost({
 //         data: {
 //             ...data
-//         }, 
+//         },
 //         where: {
 //             id: postId
 //         }
-//     }, "{  author { id name email posts { id title published } } }") 
+//     }, "{  author { id name email posts { id title published } } }")
 //     return post.author
 // }
 
 // updatePostForUser("ckku0wp0c00di0825p5rthoek", {
-//     published: true, 
+//     published: true,
 //     title: "Typescript in 3 steps"
 // }).then((user) => {
 //     console.log(JSON.stringify(user, undefined, 2))
 // }).catch((error) => {
 //     console.log(error.message)
 // })
-
 
 // createPostForUser("ckku2b7n200ye0825jb138jku", {
 //     title: "Great books to read",
@@ -79,4 +74,3 @@ export { prisma as default }
 // }).catch((error) => {
 //     console.log(error.message)
 // })
-
--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,101 +1,136 @@
-import getUserId from "../utils/getUserId"
+import getUserId from '../utils/getUserId';
 
 const Query = {
-  users(parent, args , { prisma }, info) {
-    const opArgs = {} //operation arguments
+  users(parent, args, { prisma }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
+    }; //operation arguments
 
-    if(args.query) {
+    if (args.query) {
       opArgs.where = {
-        OR: [{
-          name_contains: args.query
-        }]
-      }
+        OR: [
+          {
+            name_contains: args.query,
+          },
+        ],
+      };
     }
 
-    return prisma.query.users(opArgs, info)
+    return prisma.query.users(opArgs, info);
   },
   myPost(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request)
-    const opArgs = { 
+    const userId = getUserId(request);
+    const opArgs = {
+      fist: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
-          id: userId
-        }
-      }
-    }
-
-    if(args.query) {
-      opArgs.where.OR =[{
-        title_contains: args.query
+          id: userId,
+        },
       },
-      {
-        body_contains: args.query
-      }]
+    };
+
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
     }
 
-    if(!userId) {
-      throw new Error('Unable to authenticate')
+    if (!userId) {
+      throw new Error('Unable to authenticate');
     }
 
-    return prisma.query.posts(opArgs, info)
+    return prisma.query.posts(opArgs, info);
   },
-  posts(parent, args , { prisma }, info) {
+  posts(parent, args, { prisma }, info) {
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
-        published: true
-      }
-    } 
+        published: true,
+      },
+    };
 
-    if(args.query) {
-      opArgs.where.OR = [{
-          title_contains: args.query
-        },{
-          body_contains: args.query
-      }]
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
     }
 
-    return prisma.query.posts(opArgs, info)
+    return prisma.query.posts(opArgs, info);
   },
   comments(parent, args, { prisma }, info) {
-    return prisma.query.comments(null, info);
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
+    };
+    return prisma.query.comments(opArgs, info);
   },
   async me(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request)
+    const userId = getUserId(request);
 
-    if(!userId) {
-      throw new Error('Authentication required')
+    if (!userId) {
+      throw new Error('Authentication required');
     }
 
-    const user = await prisma.query.user({ 
-      where: {
-        id: userId
-      }
-     }, info)
-     
-    return user
+    const user = await prisma.query.user(
+      {
+        where: {
+          id: userId,
+        },
+      },
+      info
+    );
+
+    return user;
   },
   async post(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request, false)
+    const userId = getUserId(request, false);
 
-    const posts = await prisma.query.posts({
-      where: {
-        id: args.id,
-        OR: [{
-          published: true
-        }, {
-          author: {
-            id: userId
-          }
-        }]
-      }
-    }, info)
+    const posts = await prisma.query.posts(
+      {
+        where: {
+          id: args.id,
+          OR: [
+            {
+              published: true,
+            },
+            {
+              author: {
+                id: userId,
+              },
+            },
+          ],
+        },
+      },
+      info
+    );
 
-    if(posts.lenght === 0) {
-      throw new Error('Post not found')
+    if (posts.lenght === 0) {
+      throw new Error('Post not found');
     }
 
-    return posts[0]
-  }
+    return posts[0];
+  },
 };
 
-export default Query
+export default Query;
